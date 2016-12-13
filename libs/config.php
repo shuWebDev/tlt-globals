f74ca9316f17c4c323d09141fc3b444143da2d1d
G. L. Clark, II <gary.clark@shu.edu>
2016-12-13 15:20:35 -0500

    Updates global path

--- libs/config.php
+++ libs/config.php
@@ -1,7 +1,8 @@
 <?php
-	define('URI_GLOBAL_BASE', '/tlt/projects/'); // Should match what's on the server.
-	define('URI_GLOBAL_CSS', URI_GLOBAL_BASE . 'globals/assets/css/app.css');
-	define('URI_GLOBAL_ASSETS', URI_GLOBAL_BASE . 'globals/assets/');
-	define('URI_GLOBAL_VENDOR', URI_GLOBAL_BASE . 'globals/vendor/');
+	define('URI_GLOBAL_BASE', '/projects/globals'); // Should match what's on the server.
+	define('URI_GLOBAL_CSS', URI_GLOBAL_BASE . '/assets/css/app.css');
+	define('URI_GLOBAL_ASSETS', URI_GLOBAL_BASE . '/assets/');
+	define('URI_GLOBAL_VENDOR', URI_GLOBAL_BASE . '/vendor/');
 	define('PATH_GLOBAL_VIEWS_DIR', dirname(__DIR__) . '/views/');
+
 ?>
