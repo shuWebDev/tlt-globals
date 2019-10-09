a8d6467f08962d44d69d35f57b02f2c20c15ed86
G. L. Clark, II <gclark2@gmail.com>
2019-10-09 15:17:18 -0400

    Updates application nav position

--- _project-example/config/config.php
+++ _project-example/config/config.php
@@ -5,7 +5,7 @@
     $application_header = null;
     $application_header_nav = null;
     $application_nav = '/_project-example/nav-main.php';
-    $application_nav_position = 'right'; // left or right if populated.
+    $application_nav_position = 'left'; // left or right if populated.
     $application_scripts = '/_project-example/js/app.js';
     $application_styles = '/_project-example/css/app.css';
 ?>
