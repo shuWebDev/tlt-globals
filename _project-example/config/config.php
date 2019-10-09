5c49a5a985b904db7f9ab3c7047f9e895d8629b6
G. L. Clark, II <gclark2@gmail.com>
2019-10-09 13:26:14 -0400

    Adds new templates
    Two new templates have been added to display two columns. One of which has a selectable position for the nav. Styles have been added or updated to reflect the additions. Also added a configuration file for the example project.

---
+++ _project-example/config/config.php
@@ -0,0 +1,11 @@
+<?php
+    require_once(dirname(dirname(__DIR__)) . "/globals/config/config.php");
+
+    $application_footer = null;
+    $application_header = null;
+    $application_header_nav = null;
+    $application_nav = '/_project-example/nav-main.php';
+    $application_nav_position = 'right'; // left or right if populated.
+    $application_scripts = '/_project-example/js/app.js';
+    $application_styles = '/_project-example/css/app.css';
+?>
