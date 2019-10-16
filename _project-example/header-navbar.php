b9450caecde149f788fe54dd8a863d1983e8532f
G. L. Clark, II <gclark2@gmail.com>
2019-10-16 16:19:38 -0400

    Removes search form from project navbar.

--- _project-example/header-navbar.php
+++ _project-example/header-navbar.php
@@ -3,7 +3,3 @@
     <li class="nav-item"><a class="nav-link" href="#">Link</a></li>
     <li class="nav-item"><a class="nav-link disabled" href="#">Disabled</a></li>
 </ul>
-
-<form class="form-inline my-2 my-md-0">
-    <input class="form-control" type="text" placeholder="Search" aria-label="Search">
-</form>
