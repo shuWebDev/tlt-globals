cbc14cef9748314a8290c795a6dddae5c3855dfc
tommcgee57 <mcgeetho@shu.edu>
2019-11-08 14:54:58 -0500

    Update index.php
    Replace buffering with appended variables.

--- _project-example/subfolder1/subfolder1a/index.php
+++ _project-example/subfolder1/subfolder1a/index.php
@@ -7,33 +7,35 @@
     $header_type = "full";
 
     //* NOTE: Start custom code for the main content here.
-    ob_start();
+    // ob_start();
+    $content_primary = '';
 
     $x = 5985;
     // var_dump($x);
-    echo '<p>The value of $x is: ' . $x . '</p>';
+    $content_primary .= '<p>The value of $x is: ' . $x . '</p>';
 
     $cars = array("Volvo","BMW","Toyota");
     // var_dump($cars);
-    echo '<p>The second value of $cars is: ' . $cars[1] . '</p>';
+    $content_primary .= '<p>The second value of $cars is: ' . $cars[1] . '</p>';
 
-    echo '<ul>';
+    $content_primary .= '<ul>';
     foreach ($cars as $car) {
-        echo "<li class=\"car\">$car</li>";
+        $content_primary .= "<li class=\"car\">$car</li>";
     }
-    echo '</ul>';
+    $content_primary .= '</ul>';
 
     //* NOTE: End custom code for primary content here.
-    $content_primary = ob_get_clean();
+    // $content_primary = ob_get_clean();
 
     //* NOTE: Start custom code for the aside content here.
-    ob_start();
-    ?>
-
+    //ob_start();
+    
+    $content_secondary = <<<END_SECONDARY
     <h3>What's This?</h3>
     <p>This is generic HTML content that is in a second column.</p>
 
-    <?php
+END_SECONDARY;
+
     //* NOTE: End custom code for secondary content here.
     $content_secondary = ob_get_clean();
 
